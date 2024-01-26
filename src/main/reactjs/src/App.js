c12168f2003007e0a6c701eae93008954138470d
moonhee <zipsy327@gmail.com>
2024-01-26 17:48:16 +0900

    second

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <img alt='' src={mainImg}/>
       <h2><b>스프링부트+리액트 CI/CD 성공기원!!!! <br/>한번에 성공하기</b></h2>
+      <h2><b>성공했습니다!!</b></h2>
     </div>
   );
 }
